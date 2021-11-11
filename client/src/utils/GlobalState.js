61dcc4416a8434f9d55d25935d9414685a129794
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-11-10 16:42:11 -0800

    cart UI

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,21 +1,22 @@
 import React, { createContext, useContext } from "react";
-import { useProductReducer } from './reducers';
+import { useProductReducer } from "./reducers";
 
 const StoreContext = createContext();
 const { Provider } = StoreContext;
 
 const StoreProvider = ({ value = [], ...props }) => {
-    const [state, dispatch] = useProductReducer({
-      products: [],
-      categories: [],
-      currentCategory: '',
-    });
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    cart: [],
+    cartOpen: false,
+    categories: [],
+    currentCategory: "",
+  });
+  return <Provider value={[state, dispatch]} {...props} />;
+};
 
-    return <Provider value={[state, dispatch]} {...props} />;
-  };
-  
-  const useStoreContext = () => {
-    return useContext(StoreContext);
-  };
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
 
-  export { StoreProvider, useStoreContext };
+export { StoreProvider, useStoreContext };
